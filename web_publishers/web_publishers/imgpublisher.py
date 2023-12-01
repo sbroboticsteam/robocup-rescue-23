a8665328baff31ad849cfb041b80a71220881829
Giovanni <giovanni.terefi@stonybrook.edu>
2023-12-01 16:14:55 -0500

    Image publisher works now

--- web_publishers/web_publishers/imgpublisher.py
+++ web_publishers/web_publishers/imgpublisher.py
@@ -5,7 +5,7 @@ from cv_bridge import CvBridge
 import cv2
 import base64
 
-IMG_PATH = 'sample.jpg'
+IMG_PATH = 'web_publishers/sample.jpg'
 
 class ImagePublisher(Node):
     def __init__(self):
@@ -22,6 +22,7 @@ class ImagePublisher(Node):
         _, buffer = cv2.imencode('.jpg', cv_image)
         jpgAsText = base64.b64encode(buffer).decode()
         self.publisher_.publish(String(data=jpgAsText))
+        self.get_logger().info('Publishing: "%s"' % IMG_PATH)
 
 def main(args=None):
     rclpy.init(args=args)
